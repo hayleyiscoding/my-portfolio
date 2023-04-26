52dd0fc2ae9dd86f12e9019056e83710ba64e3b9
Hayley <hayleyiscoding@gmail.com>
2023-04-26 18:03:01 +1000

    update navbar hamburger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,10 @@ import {
 } from "react-icons/ai";
 import { SiHashnode } from "react-icons/si";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
     <>
       <nav className="fixed top-0 z-50 w-full bg-black border-b border-black dark:bg-gray-800 dark:border-gray-700">
@@ -17,12 +19,13 @@ const Navbar = () => {
                 data-drawer-target="logo-sidebar"
                 data-drawer-toggle="logo-sidebar"
                 aria-controls="logo-sidebar"
+                onClick={() => setShowSidebar((prevState) => !prevState)}
                 type="button"
-                className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               >
                 <span className="sr-only">Open sidebar</span>
                 <svg
-                  className="w-6 h-6"
+                  className="w-4 h-6"
                   aria-hidden="true"
                   fill="currentColor"
                   viewBox="0 0 20 20"
@@ -36,7 +39,7 @@ const Navbar = () => {
                 </svg>
               </button>
               <Link to="/home" className="flex md:mr-24">
-                <div className="h-8 mr-3 ml-2 bg-custom-red text-white p-1">
+                <div className="h-8 mr-3 ml-3 md:ml-2 bg-custom-red text-white p-1">
                   H
                 </div>
                 <span className="text-white self-center tracking-wide font-bold sm:text-md whitespace-nowrap dark:text-white">
@@ -172,7 +175,9 @@ const Navbar = () => {
 
       <aside
         id="logo-sidebar"
-        className="fixed top-0 z-40 w-8 h-screen pt-20 transition-transform -translate-x-full bg-black border-r border-black sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700 "
+        className={`fixed hidden top-0 z-40 w-8 h-screen pt-20 transition-transform -translate-x-full bg-black border-r border-black sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700 ${
+          showSidebar ? "visible" : "hidden"
+        }`}
         aria-label="Sidebar"
       >
         <div className="h-full bg-black dark:bg-gray-800">
@@ -243,7 +248,7 @@ const Navbar = () => {
                   <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
                   <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
                 </svg> */}
-                <span className="-rotate-90">Core</span>
+                <span className="-rotate-90">Technology</span>
                 {/* <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
                   3
                 </span> */}
@@ -340,6 +345,177 @@ const Navbar = () => {
           </ul>
         </div>
       </aside>
+
+      {/* mobile menu */}
+
+      <div
+        className={`h-full bg-custom-black dark:bg-custom-black ${
+          showSidebar ? "md:visible" : "hidden"
+        }`}
+      >
+        <ul className="font-light flex flex-col justify-end space-y-12">
+          {/* <li>
+              <as
+                href="#"
+                className="flex  p-2 text-white rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <span className="ml-8 -rotate-90">Dashboard</span>
+              </a>
+            </li> */}
+
+          <li className="">
+            <Link
+              to="/home"
+              className="flex flex-col items-center text-white rounded-lg dark:text-white hover:text-custom-red dark:hover:bg-gray-700 mt-3"
+            >
+              {/* <svg
+                  aria-hidden="true"
+                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+                </svg> */}
+              <span className="">Home</span>
+              {/* <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
+                  Pro
+                </span> */}
+            </Link>
+          </li>
+
+          <li className="">
+            <Link
+              to="/home#about"
+              className="flex flex-col items-center p-2 text-white rounded-lg dark:text-white hover:text-custom-red dark:hover:bg-gray-700 focus:text-custom-red"
+            >
+              {/* <svg
+                  aria-hidden="true"
+                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+                </svg> */}
+              <span className="">About</span>
+              {/* <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
+                  Pro
+                </span> */}
+            </Link>
+          </li>
+
+          <li className="">
+            <Link
+              to="/home#core-skills"
+              className="flex flex-col items-center text-white rounded-lg dark:text-white hover:text-custom-red dark:hover:bg-gray-700"
+            >
+              {/* <svg
+                  aria-hidden="true"
+                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
+                  <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
+                </svg> */}
+              <span className="">Technology</span>
+              {/* <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
+                  3
+                </span> */}
+            </Link>
+          </li>
+
+          <li className="">
+            <Link
+              to="#education"
+              className="flex flex-col items-center text-white rounded-lg dark:text-white hover:text-custom-red  dark:hover:bg-gray-700"
+            >
+              {/* <svg
+                  aria-hidden="true"
+                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg> */}
+              <span className="">Education</span>
+            </Link>
+          </li>
+
+          <li className="">
+            <a
+              href="#"
+              className="flex flex-col items-center text-white rounded-lg dark:text-white hover:text-custom-red dark:hover:bg-gray-700"
+            >
+              {/* <svg
+                  aria-hidden="true"
+                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg> */}
+              <span className="">Experience</span>
+            </a>
+          </li>
+
+          <li className="">
+            <Link
+              to="/home/projects"
+              className="flex flex-col items-center text-white rounded-lg dark:text-white hover:text-custom-red dark:hover:bg-gray-700"
+            >
+              {/* <svg
+                  aria-hidden="true"
+                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg> */}
+              <span className="">Projects</span>
+            </Link>
+          </li>
+
+          <li className="">
+            <a
+              href="#"
+              className="flex flex-col items-center text-white rounded-lg dark:text-white hover:text-custom-red dark:hover:bg-gray-700"
+            >
+              {/* <svg
+                  aria-hidden="true"
+                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg> */}
+              <span className="">Contact</span>
+            </a>
+          </li>
+        </ul>
+      </div>
 
       <div className="p-4 sm:ml-64 bg-custom-black">
         {/* <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
