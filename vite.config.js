bceab7f88a797f6d4fe8afec2d3862d32cf5b85f
Hayley <hayleyiscoding@gmail.com>
2023-05-01 20:36:41 +1000

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // rollupOptions: {
-    //   external: [/^\/artifacts\/.*\.json$/],
-    // },
-    chunkSizeWarningLimit: 1000,
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: [/^\/artifacts\/.*\.json$/],
+  //   },
+  //   chunkSizeWarningLimit: 1000,
+  // },
   optimizeDeps: {
     exclude: ["swiper"],
   },
