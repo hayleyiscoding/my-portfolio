581efa5556e2d19d544b72438faf97b4b0d3088f
Hayley <hayleyiscoding@gmail.com>
2023-05-02 00:55:23 +1000

    update abi folder

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [/^\/artifacts\/.*\.json$/],
-    },
-    // chunkSizeWarningLimit: 800,
-  },
   optimizeDeps: {
     exclude: ["swiper"],
   },
