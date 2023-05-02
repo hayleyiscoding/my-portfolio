22abc534be92b36befa2f2f2164bf1b0d56b4f8c
Hayley <hayleyiscoding@gmail.com>
2023-05-02 22:53:55 +1000

    reconnect contract in guestbook

--- src/components/Guestbook.jsx
+++ src/components/Guestbook.jsx
@@ -1,16 +1,41 @@
 import { useState, useEffect } from "react";
 import { BiDownArrowAlt } from "react-icons/bi";
 import { ethers } from "ethers";
-import GuestbookABI from "../assets/abi/Guestbook.json";
+import GuestbookABI from "/src/assets/abi/Guestbook.json";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import {
+  useAccount,
+  useDisconnect,
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+} from "wagmi";
 
-// deployed on polygon mainnet from 'web3' wallet
+// Contract Address - deployed on polygon mainnet from 'web3' wallet
 const guestbookContractAddress = "0x00F8e2B75e754107D02D03bf0bbdfD9934e35631";
 
+// abi
+const abi = GuestbookABI.abi;
+
+// Provider
+// const providerUrl = `https://polygon-mainnet.g.alchemy.com/v2/${alchemyKey}`;
+// const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+// Contract Instance
+const guestBookContract = new ethers.Contract(
+  guestbookContractAddress,
+  abi,
+  provider
+);
+
 const Guestbook = () => {
+  const { data: account } = useAccount();
+  const { disconnect } = useDisconnect();
+
   const [message, setMessageValue] = useState("");
   const [messages, setMessages] = useState([]);
   const [walletAddress, setWalletAddress] = useState("");
-  const [isConnected, setIsConnected] = useState(false);
 
   const requestAccount = async () => {
     if (window.ethereum) {
@@ -21,7 +46,6 @@ const Guestbook = () => {
           method: "eth_requestAccounts",
         });
         setWalletAddress(accounts[0]);
-        setIsConnected((prevState) => !prevState);
       } catch (error) {
         console.log("Error connecting...");
       }
@@ -30,23 +54,40 @@ const Guestbook = () => {
     }
   };
 
-  const connectWallet = async () => {
-    if (typeof window.ethereum !== "undefined") {
-      await requestAccount();
-    }
-  };
-
   useEffect(() => {
     fetchMessages();
   }, []);
 
+  const {
+    data: readData,
+    isError,
+    isLoading: isLoadingRead,
+  } = useContractRead({
+    address: guestbookContractAddress,
+    abi: abi,
+    functionName: "getAllMessages",
+  });
+
+  const { config } = usePrepareContractWrite({
+    address: guestbookContractAddress,
+    abi: abi,
+    functionName: "setMessage",
+  });
+  const {
+    data,
+    isLoading: isLoadingWrite,
+    isSuccess,
+    write,
+  } = useContractWrite(config);
+
   const fetchMessages = async () => {
     if (typeof window.ethereum !== "undefined") {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
       const contract = new ethers.Contract(
         guestbookContractAddress,
         GuestbookABI.abi,
-        provider
+        signer
       );
       try {
         const data = await contract.getAllMessages();
@@ -108,21 +149,38 @@ const Guestbook = () => {
         </div>
         <div>
           <p className="mt-12 mb-8 text-sm md:text-md font-light leading-5 text-white text-opacity-70 max-w-sm mx-auto text-center px-5">
-            Do you have a Metamask wallet? Please sign my guestbook!
+            Do you have a Metamask wallet? I'd love it if you signed my
+            guestbook!
             <br />
             <br />
             Please note that this guestbook has been created on the Polygon
             Network (blockchain), so you will pay a tiny gas fee in MATIC
             (perhaps US$0.01 or less) when signing. Thanks!
           </p>
-          <div className="text-center mb-10 mt-0">
-            <button
+          <div className="flex justify-center text-center mb-10 mt-0">
+            {/* <button
               type="submit"
               onClick={connectWallet}
               className="cursor-pointer inline-flex font-light items-center justify-center px-6 py-3 mt-0 text-sm leading-5 text-white transition-all duration-200 bg-custom-red border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black hover:bg-black hover:text-white"
             >
               {walletAddress ? "Connected" : "Connect Wallet"}
-            </button>
+            </button> */}
+
+            {account ? (
+              <button>
+                <p>{account.address}</p>
+              </button>
+            ) : (
+              <>
+                <div>
+                  <ConnectButton disabled={!write} onClick={() => write?.()} />
+                </div>
+                {/* <div>
+                  {isLoadingWrite && <div>Check Wallet</div>}
+                  {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
+                </div> */}
+              </>
+            )}
           </div>
 
           <form onSubmit={handleSubmit} className=" bg-black max-w-sm mx-auto">
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,41 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { polygon } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+// import dotenv from "dotenv";
+// dotenv.config({ path: "./.env.local" });
+
+const alchemyId = import.meta.env.VITE_ALCHEMY_API_KEY;
+
+const { chains, provider } = configureChains(
+  [polygon],
+  [alchemyProvider({ alchemyId }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Hayley's Portfolio",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <App />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </React.StrictMode>
   </BrowserRouter>
 );
